6d5e3796e1bbd8d18cb1b88047ee6ea41d9ee1f4
Amar <badnja25@hotmail.com>
2019-04-30 12:45:28 +0200

    Added form and functionality for sending email to admins.
     - Designed UI for sending email.
     - Added Send_Email class.
     - Coded functionality for sending email after "Pošalji" button is clicked.

---
+++ eDnevnik/eDnevnik/Send Email.Designer.vb
@@ -0,0 +1,136 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Send_Email
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Send_Email))
+        Me.TxtSubject = New System.Windows.Forms.TextBox()
+        Me.txtMessage = New System.Windows.Forms.RichTextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.BtnSend = New System.Windows.Forms.Button()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.ButtonNazad = New System.Windows.Forms.Button()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'TxtSubject
+        '
+        Me.TxtSubject.Location = New System.Drawing.Point(151, 12)
+        Me.TxtSubject.Name = "TxtSubject"
+        Me.TxtSubject.Size = New System.Drawing.Size(257, 20)
+        Me.TxtSubject.TabIndex = 1
+        '
+        'txtMessage
+        '
+        Me.txtMessage.Location = New System.Drawing.Point(8, 82)
+        Me.txtMessage.Name = "txtMessage"
+        Me.txtMessage.Size = New System.Drawing.Size(467, 161)
+        Me.txtMessage.TabIndex = 3
+        Me.txtMessage.Text = ""
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(77, 12)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(68, 18)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "Predmet:"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(13, 61)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(60, 18)
+        Me.Label4.TabIndex = 7
+        Me.Label4.Text = "Poruka:"
+        '
+        'BtnSend
+        '
+        Me.BtnSend.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BtnSend.Location = New System.Drawing.Point(393, 247)
+        Me.BtnSend.Name = "BtnSend"
+        Me.BtnSend.Size = New System.Drawing.Size(82, 30)
+        Me.BtnSend.TabIndex = 8
+        Me.BtnSend.Text = "Pošalji"
+        Me.BtnSend.UseVisualStyleBackColor = True
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = Global.eDnevnik.My.Resources.Resources.email_64x64
+        Me.PictureBox1.Location = New System.Drawing.Point(414, 12)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(64, 64)
+        Me.PictureBox1.TabIndex = 9
+        Me.PictureBox1.TabStop = False
+        '
+        'ButtonNazad
+        '
+        Me.ButtonNazad.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ButtonNazad.Image = Global.eDnevnik.My.Resources.Resources.back_32x32
+        Me.ButtonNazad.ImageAlign = System.Drawing.ContentAlignment.MiddleRight
+        Me.ButtonNazad.Location = New System.Drawing.Point(16, 247)
+        Me.ButtonNazad.Name = "ButtonNazad"
+        Me.ButtonNazad.Size = New System.Drawing.Size(76, 30)
+        Me.ButtonNazad.TabIndex = 26
+        Me.ButtonNazad.Text = "Nazad"
+        Me.ButtonNazad.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.ButtonNazad.UseVisualStyleBackColor = True
+        '
+        'Send_Email
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.ClientSize = New System.Drawing.Size(485, 287)
+        Me.Controls.Add(Me.ButtonNazad)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.BtnSend)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.txtMessage)
+        Me.Controls.Add(Me.TxtSubject)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.MaximumSize = New System.Drawing.Size(505, 330)
+        Me.MinimumSize = New System.Drawing.Size(505, 330)
+        Me.Name = "Send_Email"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Send Email"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents TxtSubject As TextBox
+    Friend WithEvents txtMessage As RichTextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents BtnSend As Button
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents ButtonNazad As Button
+End Class
