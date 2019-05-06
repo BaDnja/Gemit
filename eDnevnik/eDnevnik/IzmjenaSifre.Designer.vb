c9344a16b16cd3ae041004283fdd261fe384515f
Amar <badnja25@hotmail.com>
2019-05-06 19:11:06 +0200

    Design and functionality for changing password form.
     - User is asked for new password and to confirm password.
     - A few steps of validation are processed.

---
+++ eDnevnik/eDnevnik/IzmjenaSifre.Designer.vb
@@ -0,0 +1,154 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class IzmjenaSifre
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(IzmjenaSifre))
+        Me.txt1 = New System.Windows.Forms.TextBox()
+        Me.txt2 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.btnSave = New System.Windows.Forms.Button()
+        Me.ButtonNazad = New System.Windows.Forms.Button()
+        Me.RichTextBox1 = New System.Windows.Forms.RichTextBox()
+        Me.ButtonEye = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'txt1
+        '
+        Me.txt1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txt1.Location = New System.Drawing.Point(139, 9)
+        Me.txt1.Name = "txt1"
+        Me.txt1.Size = New System.Drawing.Size(173, 22)
+        Me.txt1.TabIndex = 0
+        Me.txt1.UseSystemPasswordChar = True
+        '
+        'txt2
+        '
+        Me.txt2.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txt2.Location = New System.Drawing.Point(139, 47)
+        Me.txt2.Name = "txt2"
+        Me.txt2.Size = New System.Drawing.Size(173, 22)
+        Me.txt2.TabIndex = 1
+        Me.txt2.UseSystemPasswordChar = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(10, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(113, 16)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Unesite novu šifru"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(35, 47)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(88, 16)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Potvrdite šifru"
+        '
+        'btnSave
+        '
+        Me.btnSave.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnSave.Image = Global.eDnevnik.My.Resources.Resources.password_32x32
+        Me.btnSave.ImageAlign = System.Drawing.ContentAlignment.MiddleRight
+        Me.btnSave.Location = New System.Drawing.Point(139, 75)
+        Me.btnSave.Name = "btnSave"
+        Me.btnSave.Size = New System.Drawing.Size(93, 36)
+        Me.btnSave.TabIndex = 4
+        Me.btnSave.Text = "Spremi"
+        Me.btnSave.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.btnSave.UseVisualStyleBackColor = True
+        '
+        'ButtonNazad
+        '
+        Me.ButtonNazad.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ButtonNazad.Image = Global.eDnevnik.My.Resources.Resources.back_32x32
+        Me.ButtonNazad.ImageAlign = System.Drawing.ContentAlignment.MiddleRight
+        Me.ButtonNazad.Location = New System.Drawing.Point(250, 75)
+        Me.ButtonNazad.Name = "ButtonNazad"
+        Me.ButtonNazad.Size = New System.Drawing.Size(84, 36)
+        Me.ButtonNazad.TabIndex = 49
+        Me.ButtonNazad.Text = "Nazad"
+        Me.ButtonNazad.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.ButtonNazad.UseVisualStyleBackColor = True
+        '
+        'RichTextBox1
+        '
+        Me.RichTextBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.RichTextBox1.Location = New System.Drawing.Point(12, 114)
+        Me.RichTextBox1.Name = "RichTextBox1"
+        Me.RichTextBox1.ReadOnly = True
+        Me.RichTextBox1.Size = New System.Drawing.Size(319, 58)
+        Me.RichTextBox1.TabIndex = 50
+        Me.RichTextBox1.Text = "Šifra mora sadržavati minimalno osam karaktera. U suprotnom, šifra neće biti prih" &
+    "vaćena!"
+        '
+        'ButtonEye
+        '
+        Me.ButtonEye.BackColor = System.Drawing.SystemColors.AppWorkspace
+        Me.ButtonEye.BackgroundImage = Global.eDnevnik.My.Resources.Resources.eye_32x32
+        Me.ButtonEye.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center
+        Me.ButtonEye.Location = New System.Drawing.Point(318, 25)
+        Me.ButtonEye.Name = "ButtonEye"
+        Me.ButtonEye.Size = New System.Drawing.Size(38, 38)
+        Me.ButtonEye.TabIndex = 51
+        Me.ButtonEye.UseVisualStyleBackColor = False
+        '
+        'IzmjenaSifre
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(369, 175)
+        Me.ControlBox = False
+        Me.Controls.Add(Me.ButtonEye)
+        Me.Controls.Add(Me.RichTextBox1)
+        Me.Controls.Add(Me.ButtonNazad)
+        Me.Controls.Add(Me.btnSave)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txt2)
+        Me.Controls.Add(Me.txt1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.Name = "IzmjenaSifre"
+        Me.Text = "Izmjena Šifre"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txt1 As TextBox
+    Friend WithEvents txt2 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents btnSave As Button
+    Friend WithEvents ButtonNazad As Button
+    Friend WithEvents RichTextBox1 As RichTextBox
+    Friend WithEvents ButtonEye As Button
+End Class
---
+++ eDnevnik/eDnevnik/IzmjenaSifre.vb
@@ -0,0 +1,61 @@
+﻿Public Class IzmjenaSifre
+
+    Private Sub clearAllText()
+        txt1.Clear()
+        txt2.Clear()
+    End Sub
+
+    Private Function validation() As Boolean
+        Dim value As Boolean = False
+
+        If txt1.Text <> String.Empty And txt2.Text <> String.Empty Then
+            If txt1.Text.Length >= 8 Then
+                If txt2.Text = txt1.Text Then
+                    value = True
+                Else
+                    MessageBox.Show("Šifre se ne podudaraju!")
+                    clearAllText()
+                End If
+            Else
+                MessageBox.Show("Šifra mora sadržavati minimalno osam znakova!")
+                clearAllText()
+            End If
+        Else
+            MessageBox.Show("Polja ne mogu biti prazna!")
+        End If
+        Return value
+    End Function
+
+    Private Sub updatePassword()
+        Dim sql As String = String.Format("UPDATE tbl_user SET user_password='{0}'", Encryption.hashString(txt1.Text, Profil.txtJmbg.Text))
+
+        If Form2Admin.executeNonQuery(sql) = True Then
+            MsgBox("Ažuriranje šifre uspjelo!")
+            clearAllText()
+        End If
+    End Sub
+
+    Private Sub ButtonNazad_Click_1(sender As Object, e As EventArgs) Handles ButtonNazad.Click
+        clearAllText()
+        Me.Hide()
+        Profil.Show()
+    End Sub
+
+    Private Sub btnSave_Click(sender As Object, e As EventArgs) Handles btnSave.Click
+        If validation() = True Then
+            updatePassword()
+        End If
+    End Sub
+
+    Private Sub ButtonEye_MouseEnter(sender As Object, e As EventArgs) Handles ButtonEye.MouseEnter
+
+        txt1.UseSystemPasswordChar = False
+        txt2.UseSystemPasswordChar = False
+    End Sub
+
+    Private Sub ButtonEye_MouseLeave(sender As Object, e As EventArgs) Handles ButtonEye.MouseLeave
+
+        txt1.UseSystemPasswordChar = True
+        txt2.UseSystemPasswordChar = True
+    End Sub
+End Class
