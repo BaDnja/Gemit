e1b47814976870914916eff9176b3709d507a638
Badnja <badnja25@hotmail.com>
2019-05-07 15:01:09 +0200

    Design and functionality for "UpitProfesor" Form
     - Form uses professors ID to check if he/she choosed subjects.
     - If not, Form opens, in other case, "Profil" will be opened.
    
     - Professor can choose max. five subjects.

---
+++ eDnevnik/eDnevnik/UpitProfesor.Designer.vb
@@ -0,0 +1,139 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class UpitProfesor
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(UpitProfesor))
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.btnAdd = New System.Windows.Forms.Button()
+        Me.txtSubject = New System.Windows.Forms.TextBox()
+        Me.DataView = New System.Windows.Forms.DataGridView()
+        Me.btnOpenProfile = New System.Windows.Forms.Button()
+        Me.txtSearch = New System.Windows.Forms.TextBox()
+        Me.btnSearch = New System.Windows.Forms.Button()
+        CType(Me.DataView, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(12, 66)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(125, 16)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Odaberi predmet"
+        '
+        'btnAdd
+        '
+        Me.btnAdd.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnAdd.Location = New System.Drawing.Point(284, 31)
+        Me.btnAdd.Name = "btnAdd"
+        Me.btnAdd.Size = New System.Drawing.Size(101, 42)
+        Me.btnAdd.TabIndex = 1
+        Me.btnAdd.Text = "Dodaj predmet"
+        Me.btnAdd.UseVisualStyleBackColor = True
+        '
+        'txtSubject
+        '
+        Me.txtSubject.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtSubject.Location = New System.Drawing.Point(15, 41)
+        Me.txtSubject.Name = "txtSubject"
+        Me.txtSubject.ReadOnly = True
+        Me.txtSubject.Size = New System.Drawing.Size(256, 22)
+        Me.txtSubject.TabIndex = 2
+        '
+        'DataView
+        '
+        Me.DataView.AllowUserToAddRows = False
+        Me.DataView.AllowUserToDeleteRows = False
+        Me.DataView.BackgroundColor = System.Drawing.SystemColors.ActiveCaption
+        Me.DataView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataView.Location = New System.Drawing.Point(12, 93)
+        Me.DataView.Name = "DataView"
+        Me.DataView.ReadOnly = True
+        Me.DataView.Size = New System.Drawing.Size(159, 307)
+        Me.DataView.TabIndex = 3
+        '
+        'btnOpenProfile
+        '
+        Me.btnOpenProfile.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnOpenProfile.Location = New System.Drawing.Point(284, 370)
+        Me.btnOpenProfile.Name = "btnOpenProfile"
+        Me.btnOpenProfile.Size = New System.Drawing.Size(97, 30)
+        Me.btnOpenProfile.TabIndex = 4
+        Me.btnOpenProfile.Text = "Otvori profil"
+        Me.btnOpenProfile.UseVisualStyleBackColor = True
+        '
+        'txtSearch
+        '
+        Me.txtSearch.Location = New System.Drawing.Point(177, 103)
+        Me.txtSearch.Name = "txtSearch"
+        Me.txtSearch.Size = New System.Drawing.Size(189, 20)
+        Me.txtSearch.TabIndex = 5
+        '
+        'btnSearch
+        '
+        Me.btnSearch.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnSearch.Image = Global.eDnevnik.My.Resources.Resources.search_32x32
+        Me.btnSearch.ImageAlign = System.Drawing.ContentAlignment.MiddleRight
+        Me.btnSearch.Location = New System.Drawing.Point(225, 129)
+        Me.btnSearch.Name = "btnSearch"
+        Me.btnSearch.Size = New System.Drawing.Size(141, 38)
+        Me.btnSearch.TabIndex = 6
+        Me.btnSearch.Text = "Traži predmet"
+        Me.btnSearch.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.btnSearch.UseVisualStyleBackColor = True
+        '
+        'UpitProfesor
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.ClientSize = New System.Drawing.Size(390, 409)
+        Me.ControlBox = False
+        Me.Controls.Add(Me.btnSearch)
+        Me.Controls.Add(Me.txtSearch)
+        Me.Controls.Add(Me.btnOpenProfile)
+        Me.Controls.Add(Me.DataView)
+        Me.Controls.Add(Me.txtSubject)
+        Me.Controls.Add(Me.btnAdd)
+        Me.Controls.Add(Me.Label1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.Name = "UpitProfesor"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Upit Profesora"
+        CType(Me.DataView, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents btnAdd As Button
+    Friend WithEvents txtSubject As TextBox
+    Friend WithEvents DataView As DataGridView
+    Friend WithEvents btnOpenProfile As Button
+    Friend WithEvents txtSearch As TextBox
+    Friend WithEvents btnSearch As Button
+End Class
---
+++ eDnevnik/eDnevnik/UpitProfesor.vb
@@ -0,0 +1,85 @@
+﻿Imports System.Data.SQLite
+
+Public Class UpitProfesor
+    Dim database As New Database
+    Dim conn As String = database.connectionString
+    Dim brojac As Integer = 0
+
+    ' Sub for loading data from data grid view into textboxes
+    Private Sub CellMouseClick(ByVal sender As Object, ByVal e As System.Windows.Forms.DataGridViewCellMouseEventArgs) Handles DataView.CellMouseClick
+        On Error Resume Next
+        txtSubject.Text = DataView.Rows(e.RowIndex).Cells(0).Value
+    End Sub
+
+    Private Sub loadSubjects()
+        Dim connection As New SQLiteConnection(conn)
+        Try
+            connection.Open()
+        Catch ex As Exception
+            connection.Dispose()
+            conn = Nothing
+            MsgBox(ex.Message)
+            Exit Sub
+        End Try
+
+        Dim dt As New DataTable
+        Try
+            If txtSearch.Text.Length <> 0 Then
+                Form2Admin.daFill(String.Format("SELECT subject_name FROM tbl_subject WHERE user_id is null AND subject_name='{0}' COLLATE NOCASE;", txtSearch.Text), dt)
+            Else
+                Form2Admin.daFill("SELECT subject_name FROM tbl_subject WHERE user_id is null;", dt)
+            End If
+            DataView.DataSource = Nothing
+            DataView.DataSource = dt
+        Catch ex As Exception
+            MsgBox("Greška pri učitavanju predmeta; Poruka: " + ex.Message)
+        End Try
+
+        dt.Dispose()
+        dt = Nothing
+        connection.Close()
+        connection.Dispose()
+        connection = Nothing
+    End Sub
+
+    Private Sub updateUserId()
+        Dim sql As String = String.Format("UPDATE tbl_subject SET user_id='{0}' WHERE subject_name='{1}';", Profil.m_user_id, txtSubject.Text)
+
+        Try
+            Profil.executeNonQuery(sql)
+        Catch ex As Exception
+            MessageBox.Show("Greška pri upisivanju korisničkog ID-a; Poruka: " + ex.Message)
+        End Try
+    End Sub
+
+    Private Sub UpitProfesor_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        loadSubjects()
+    End Sub
+
+    Private Sub btnAdd_Click(sender As Object, e As EventArgs) Handles btnAdd.Click
+        If brojac < 5 Then
+            brojac += 1
+            If txtSubject.Text <> String.Empty Then
+                updateUserId()
+                loadSubjects()
+                txtSubject.Clear()
+            Else
+                MessageBox.Show("Odaberite predmet a zatim kliknite dugme.")
+            End If
+        Else
+            MessageBox.Show("Odabrali ste maksimalno 5 predmeta!")
+            Me.Hide()
+            Profil.Show()
+        End If
+
+    End Sub
+
+    Private Sub btnSearch_Click(sender As Object, e As EventArgs) Handles btnSearch.Click
+        loadSubjects()
+    End Sub
+
+    Private Sub btnOpenProfile_Click(sender As Object, e As EventArgs) Handles btnOpenProfile.Click
+        Me.Hide()
+        Profil.Show()
+    End Sub
+End Class
